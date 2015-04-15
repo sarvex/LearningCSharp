ef89dae29723e2d1ccbb85bff3e6a6238935ebd1
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-04-15 12:52:49 +0530

    Operator Overloading

---
+++ LearningCSharpTests/PointTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningCSharp;
+using Xunit;
+
+namespace LearningCSharp.Tests
+{
+    public class PointTests 
+    {
+        [Fact ()]
+        public void AdditionTest()
+        {
+            var p1 = new Point(1, 5);
+            var p2 = new Point(2, 6);
+
+            var p3 = p1 + p2;
+            Assert.Equal(p3, new Point(3, 11));
+
+            var p4 = p1 + 1;
+            Assert.Equal(p4, new Point(2, 6));
+        }
+
+        [Fact ()]
+        public void SubtractionTest () {
+            var p1 = new Point (1, 5);
+            var p2 = new Point (2, 6);
+
+            var p3 = p1 - p2;
+            Assert.Equal (p3, new Point (-1, -1));
+
+            var p4 = p1 - 1;
+            Assert.Equal (p4, new Point (0, 4));
+        }
+    }
+}
